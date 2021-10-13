fe71733d8635cb876b99a7d560e48899c065972a
tnyandoro <tendai1@hotmail.com>
2021-10-13 07:53:34 +0200

    Add seeding data

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+
+greeting = Greeting.create([
+  {
+    "id": 1,
+    "title": 'Howzit buddy'
+  },
+  {
+    "id": 2,
+    "title": 'Greetings'
+  },
+  {
+    "id": 3,
+    "title": 'Howde mate'
+  },
+  {
+    "id": 4,
+    "title": 'Hey buddy how are you'
+  },
+  {
+    "id": 5,
+    "title": 'Welcome Buddy'
+  },
+  {
+    "id": 6,
+    "message": 'Hello Friend'
+  }
+                               ])
+

e7c1b1308e55c8cad55fa5ae12a33da38958eed9
tnyandoro <tendai1@hotmail.com>
2021-10-13 08:17:23 +0200

    Add a static index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+root 'static#index'
 namespace :v1, defaults: { format: :json } do
   get 'greetings', to: 'greetings#index'
   end
